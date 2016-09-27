695b1e3e795b49e31d0a4d03e4061ca278d40a2a
Anastasia <vernidub.anastasya@yandex.ru>
2016-09-27 21:39:07 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -2,13 +2,36 @@
 int main()
 {
 	BinarySearchTree<int> tree= { 8,10,3,6,7,14,13 }; 
-	tree.find(10);
+	//Checking "insert" function
 	tree.insert(5);
-	std::cout << tree;
-	std::ofstream file("C:\\tree.txt");
-	file << tree;
-	BinarySearchTree<int> tree2;
-	std::cin >> tree2;
-	auto a = tree.find(8);
+	//Checking "find" function
+	tree.find(10);
+
+	BinarySearchTree<int> tree3;
+	//Checking ">>" operator
+	std::cin >> tree3;
+	std::ofstream file1("C:\\tree.txt");
+	//Cheking "<<" file operator
+	file1 << tree; 
+	//Cheking copy assignment operator
+	tree = tree3;
+	//Checking ">>" operator
+	std::cout << tree<<std::endl;
+	
+    auto a = tree.find(1);
 	std::cout << (*a) << std:: endl;
+	//Cheking comparison operator
+	if (tree == tree3) 
+	{
+		std::cout << "Trees are equal!" << std::endl;
+	}
+	else { std::cout << "Trees aren't equal :( " << std::endl; }
+	BinarySearchTree<int> tree2 = {1,2,3,4,5,6};
+
+	std::ifstream file2("C:\\tree33.txt");
+	//Checking ">>" file operator (which is not working by the way!) 
+	file2 >> tree2;
+	//Checking move assignment operator
+	tree2 = std::move(tree);
+	std::cout << tree2 << std::endl;
 }
