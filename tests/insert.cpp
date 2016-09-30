28a304737e6d98016742237cbca3d686c9b07415
Anastasia <vernidub.anastasya@yandex.ru>
2016-09-30 19:05:30 +0400

    Update insert.cpp

--- tests/insert.cpp
+++ tests/insert.cpp
@@ -1,1 +1,72 @@
+#include "catch.hpp"
+#include "BinarySearchTree"
+SCENARIO ("If object already exists")
+{
+    GIVEN ("object")
+    {
+        BinarySearchTree <int> tree={1,2};
+        int object=5;
+        WHEN ("insert")
+        {
+            tree.insert(object);
+            THEN ("if it in the tree")
+            {
+                REQUIRE(!tree.insert(object));
+            }
+        };
 
+    }
+}
+SCENARIO ("If object doesn't exist")
+{
+    GIVEN ("object")
+    {
+        BinarySearchTree <int> tree={1,2};
+        int object=5;
+        WHEN ("insert")
+        {
+            tree.insert(object);
+            THEN ("if it is not in the tree")
+            {
+                REQUIRE(tree.insert(object));
+            }
+        };
+
+    }
+}
+SCENARIO ("If object already exists")
+{
+    GIVEN ("object")
+    {
+        BinarySearchTree <int> tree={1,2};
+        int object=5;
+        auto currsize=tree.size();
+        WHEN ("insert")
+        {
+            tree.insert(object);
+            THEN ("if it in the tree")
+            {
+                REQUIRE(tree.size()==currsize);
+            }
+        };
+
+    }
+}
+SCENARIO ("If object doesn't exist")
+{
+    GIVEN ("object")
+    {
+        BinarySearchTree <int> tree={1,2};
+        int object=5;
+        auto currsize=tree.size();
+        WHEN ("insert")
+        {
+            tree.insert(object);
+            THEN ("if it is not in the tree")
+            {
+                REQUIRE(currsize++);
+            }
+        };
+
+    }
+}
